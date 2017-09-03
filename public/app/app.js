9ab1bb6f98abd05dd4f8e0c60f628bd191272ff1
Alex Watanabe <alex@Alexs-MacBook-Pro.local>
2017-09-02 14:39:00 -1000

    Add script imports

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,11 @@
 var app = angular.module("oha",
         // Dependencies
-        [
+        ['underscore',
+         'ui.bootstrap',
+         'satellizer',
+         'angularModalService',
+         'angularUtils.directives.dirPagination',
+         'slickCarousel',
+         'ngSanitize',
+         'oha.routes',
        ])
