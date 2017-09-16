872112829f99e22a69ed64536ddac26e25e1da21
alexcw234 <alexcw@hawaii.edu>
2017-09-15 15:14:22 -1000

    Work on pagination

--- src/static/js/listings.js
+++ src/static/js/listings.js
@@ -4,21 +4,45 @@ var app = angular.module("oha.listings", []);
 app.controller("displaytable", function($scope, $http){
 
     $scope.headings = [];
-
-
+    $scope.has_results = -1;
     $scope.grants = [];
 
-    $http.get('/loadtable').then(function(response) {
-      response_data = response.data;
-      $scope.headings = response_data.schema
-                        .fields.map(function(item) { return item['name']; });
-      $scope.grants = response_data.data;
+    $scope.totalGrants = 0;
+    $scope.grantsPerPage = 10;
 
-    }, function(error) {
+    $scope.pagination = {
+        current: 1
+    };
 
-    });
+    var config = {
+        params: {
+            page: 1,
+            per_page: 10,
+          }
+    }
+    getResultsPage(1);
 
 
-    $scope.has_results = true;
+    function getResultsPage(pageNumber) {
+          $http.get('/loadtable', config).then(function(response) {
+            response_data = response.data;
+            $scope.headings = response_data.schema
+                              .fields.map(function(item) {
+                                          return item['name'];
+                                          });
+            $scope.grants = response_data.data;
+
+            $scope.totalGrants = response_data.Elements;
+
+            $scope.has_results = 1;
+          }, function(error) {
+
+          });
+    }
+
+    $scope.pageChanged = function(newPage) {
+      getResultsPage(newPage);
+    };
+
 
 });
