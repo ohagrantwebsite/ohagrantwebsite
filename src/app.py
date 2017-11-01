8268d46531b1087f3c4096da5f490a4c55c6d9a6
Vincent Luu <vluu7@hawaii.edu>
2017-10-31 17:39:22 -1000

    Moved old code around; initial Highcharts chart created

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template, request
 import json
-import parser_to_json
+#import parser_to_json
 import os
 app = Flask(__name__)
 
@@ -21,30 +21,30 @@ def qa():
 def contactus():
     return render_template('contactus.html')
 
-@app.route('/loadtable', methods=['post'])
-def loadtable():
-    req = request.get_json()
-    page = req['page']
-    per_page = req['per_page']
-    filters = req['filters']
-    filename = "OHA Test Data Grant_2013_2014_2015_2016_Table.xlsx"
-    dataframe = parser_to_json.parse_excel(filename, filters, page, per_page)
-    return dataframe
+#@app.route('/loadtable', methods=['post'])
+#def loadtable():
+#    req = request.get_json()
+#    page = req['page']
+#    per_page = req['per_page']
+#    filters = req['filters']
+#    filename = "OHA Test Data Grant_2013_2014_2015_2016_Table.xlsx"
+#    dataframe = parser_to_json.parse_excel(filename, filters, page, per_page)
+#    return dataframe
 
-@app.route('/loadsearch', methods=['get'])
-def loadsearch():
-    filename = "OHA Test Data Grant_2013_2014_2015_2016_Table.xlsx"
-    return parser_to_json.get_dropdowns(filename)
-
-@app.route('/loadchart', methods=['post'])
-def loadchart():
-    req = request.get_json()
-    indices = req['indices']
-    axis = req['axis']
-    mode = req['mode']
-    filename = "OHA Test Data Grant_2013_2014_2015_2016_Table.xlsx"
-    return parser_to_json.get_chart(filename, axis, indices, mode)
-
+#@app.route('/loadsearch', methods=['get'])
+#def loadsearch():
+#    filename = "OHA Test Data Grant_2013_2014_2015_2016_Table.xlsx"
+#    return parser_to_json.get_dropdowns(filename)
+#
+#@app.route('/loadchart', methods=['post'])
+#def loadchart():
+#    req = request.get_json()
+#    indices = req['indices']
+#    axis = req['axis']
+#    mode = req['mode']
+#    filename = "OHA Test Data Grant_2013_2014_2015_2016_Table.xlsx"
+#    return parser_to_json.get_chart(filename, axis, indices, mode)
+#
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
 
